bba7e9784009300ac2c18befdf9c8a56a2690000
andff <andffbr@gmail.com>
2024-08-01 16:11:17 -0300

    update

---
+++ Relogio Analógico/analog-clock-with-image-main/README.md
@@ -0,0 +1,3 @@
+# For HTML/CSS/JS
+
+Just navigate to the folder with your chosen example and open html file in the browser.
---
+++ Relogio Analógico/analog-clock-with-image-main/script.js
@@ -0,0 +1,17 @@
+//CONST
+const deg = 6;                               
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+setInterval(() => {
+    //VAR
+    let day = new Date();            
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`; //ROTATION
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
