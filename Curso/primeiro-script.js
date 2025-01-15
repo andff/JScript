a72ab26490058d83f8e0df14fa59dfa9c0803c5e
andff <andffbr@gmail.com>
2025-01-15 08:19:25 -0300

    update

---
+++ Curso/primeiro-script.js
@@ -0,0 +1,3 @@
+console.log("E eu sou o texto JavaScript scrito durante o HTML");
+console.log("... e eu sou um texto criado em um arquivo JavaScript");
+console.log("Esse foi o primeiro script desenvolvido por André Fernandes")
